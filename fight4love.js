3a5982cd471bbd03ee9327760c4bea07a17620ec
Peter Vivo <petervivo@bitbucket.org>
2017-10-05 22:39:34 +0200

    psycho gunfight done

--- fight4love.js
+++ fight4love.js
@@ -10,18 +10,23 @@ function duel(names, guns){
   const turn = () => { let gun = guns.pop(); guns.unshift(gun) }; 
   const fight = () => {
     let killers = guns.filter( (gun,i) => duelist[names[i]].gun === gun && alives.indexOf(names[i]) >= 0 );
-    alives = alives.filter( (name,i) => killers.map( killer => { 
-      let j = alives.indexOf(killer);
-      return j+1 == i || j-1 == i || ( i == 0 && j == alives.lengh-1 ) || ( j == 0 && i == alives.lengh-1 );
-    }).length === 0 );
+    alives.filter( (na,i,ar) => 
+      killers.filter( kill => { 
+        let dif = Math.abs(i-ar.indexOf(kill));
+        return dif!=alives.length-1 && dif!=1;
+      }).length == killers.length
+    )
   }  	
-  while( alives.length > 1 ){
-	fight();
-    console.log( alives.join(' - ') );
-    turn();
-  }
-  return alives.join('');
+  //while( alives.length > 1 ){
+  	const round = ()=> { fight();console.log( alives.join(' - ') );turn() };
+  //}
+  //return alives.join('');
+  round();
+  round();
+  round();
 }
+
+// duel(["Asda Btry","Csrt Dks","Gjhgj Hewr","Kewrwe Lhgj","Osdf Psde","Mretb Njhk","Isdf Jjhkhj","Eyui Ferd"],["CD","AB","MN","IJ","GH","EF","OP","KL"])
 
 let alives = [..."12345678"]
 let killers = [..."1"]
@@ -34,5 +39,64 @@ alives.filter( (na,i,ar) =>
   }).length == killers.length
 )
 
+function duel(names, guns){  
+  const getGun = name => name.replace(/[a-z]|\s/g,'');
+  const duelist = Object.assign({},...names.map( name => { 
+    let gun = getGun(name); 
+    return {[gun]:{name,gun}};
+  }));
+  let alives = [...names].map( name => getGun(name) );//console.log(alives);
+  //console.log(duelist);
+  const turn = () => { let gun = guns.pop(); guns.unshift(gun) }; 
+  const fight = () => {
+    let killers = guns.filter( (gun,i) => alives.indexOf(gun) >= 0 && duelist[getGun(names[i])].gun === gun );
+	console.log( `${alives.join('-')}\n${guns.join('-')}\n---\n${killers.join('-')}` );
+    alives = alives.filter( (na,i,ar) => 
+      killers.filter( kill => { 
+        let dif = Math.abs(i-ar.indexOf(kill));
+        return dif!=alives.length-1 && dif!=1;
+      }).length == killers.length
+    )
+  }  	
+  while( alives.length > 1 ){
+    fight();
+    turn();
+  }
+  return alives.map( gun => duelist[gun].name ).join('');
+}
 
-// duel(["Asda Btry","Csrt Dks","Gjhgj Hewr","Kewrwe Lhgj","Osdf Psde","Mretb Njhk","Isdf Jjhkhj","Eyui Ferd"],["CD","AB","MN","IJ","GH","EF","OP","KL"])
+// finally done
+function duel(names, guns){  
+  const getGun = name => name.replace(/[a-z]|\s/g,'');
+  const duelist = Object.assign({},...names.map( name => { 
+    let gun = getGun(name); 
+    return {[gun]:{name,gun}};
+  }));
+  let alives = [...names].map( name => getGun(name) );  
+  const turn = () => { let gun = guns.pop(); guns.unshift(gun) }; 
+  const fight = () => {
+    let killers = guns.filter( (gun,i) => alives.indexOf(gun) >= 0 && duelist[getGun(names[i])].gun === gun );
+    console.log( `${alives.join('-')}\n${guns.join('-')}\n---\n${killers.join('-')}` );
+    alives = alives.filter( (na, i, ar) => 
+      killers.filter( kill => { 
+        let dif = Math.abs(i-ar.indexOf(kill));
+        return dif != alives.length-1 && dif != 1;
+      }).length == killers.length
+    )
+  }  	
+  let spin = 1;
+  while( alives.length > 1 ){
+    fight();
+    for(var i=0;i<spin;i++){
+      turn();
+    }
+    spin++;
+  }
+  return alives.map( gun => duelist[gun].name ).join('');
+}
+
+duel([
+  "Tgpev Qanp","Itwxf Dqfa","Hpjgo Brno","Cahejqs Wyfif",
+  "Ffgc Rurwtj","Kfimii Xmtr","Smpym Uoyuqf","Ezlitl Vczcbu"],
+  ["TQ","CW","SU","HB","EV","FR","KX","ID"] 
+)
