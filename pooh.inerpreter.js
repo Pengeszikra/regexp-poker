340f4a4a487c340c3a18b4abcc3525fc862295e3
Peter Vivo <petervivo@bitbucket.org>
2017-10-07 20:37:49 +0200

    bracket stack v1

--- pooh.inerpreter.js
+++ pooh.inerpreter.js
@@ -1,3 +1,6 @@
+// https://www.codewars.com/kata/esoteric-language-poohbear-interpreter/train/javascript
+
+/*
 const descript = {
   '+':  "Add 1 to the current cell" ,
   '-':  "Subtract 1 from the current cell" ,
@@ -20,42 +23,85 @@ const descript = {
   Y:  "Multiply the current cell's value by the copied value" ,
   D:  "Divide the current cell's value by the copied value." ,
 }
+*/
 
-const heapSize = 50000;
-const cells = new Uint8Array( heapSize );
-const copy = new Uint8Array(1);
-const stack = [];
-const source = 'LQTcQAP>pQBBTAI-PA-PPL+P<BVPAL+T+P>PL+PBLPBP<DLLLT+P';
-const ip = 0;
-const cp = heapSize / 2;
-const outString = '';
-let out = { set out(val){ outString += val } };
+const poohbear = source => {
 
-const rules = {
-  ['+']() { cells[cp]++ },
-  ['-']() { cells[cp]-- },
-  ['>']() { cp++ },
-  ['<']() { cp-- },
-      c() { copy = cells[cp] },
-      p() { cells[cp] = copy },
-      W() { if (cells[cp] != 0){ 
-             stack.push(ip);
-           } else { 
-             while (source[++ip] === 'E' || ip>source.length ){}; 
-           } 
-         },
-      E() { ip = stack.pop() },
-      P() { out = String.fromCharCode(cells[cp]) },
-      N() { out = cells[cp] },
-      T() { cells[cp] *= 2 },
-      Q() { cells[cp] *= cells[cp] },
-      U() { cells[cp] = Math.sqrt(cells[cp]) },
-      L() { cells[cp] += 2 },
-      I() { cells[cp] -= 2 },
-      V() { cells[cp] /= 2 },
-      A() { cells[cp] += copy },
-      B() { cells[cp] -= copy },
-      Y() { cells[cp] *= copy },
-      D() { cells[cp] /= copy },
+  const memSize = 70000;
+  const cells = new Uint8Array( memSize );
+  const copy = new Uint8Array(1);
+  const stack = [];  
+  
+  let result = '';   
+  let ip = 0;
+  let cp = memSize / 2;
+
+  const rules = {
+    ['+']() { cells[cp]++ },
+    ['-']() { cells[cp]-- },
+    ['>']() { cp++ },
+    ['<']() { cp-- },
+        c() { copy[0]= cells[cp] },
+        p() { cells[cp] = copy[0] },
+        W() { if (cells[cp] != 0){ 
+              stack.push(ip);
+            } else { 
+              ip = source.indexOf('E',ip); 
+            } 
+          },
+        E() { ip = stack.pop()-1 },
+        P() { result += String.fromCharCode(cells[cp]) },
+        N() { result += cells[cp] },
+        T() { cells[cp] *= 2 },
+        Q() { cells[cp] *= cells[cp] },
+        U() { cells[cp] = Math.sqrt(cells[cp]) },
+        L() { cells[cp] += 2 },
+        I() { cells[cp] -= 2 },
+        V() { cells[cp] /= 2 },
+        A() { cells[cp] += copy[0] },
+        B() { cells[cp] -= copy[0] },
+        Y() { cells[cp] *= copy[0] },
+        D() { cells[cp] /= copy[0] },
+  }
+
+  while( ip>=0 && ip<source.length ){
+    //console.log(source[ip],result,cells[cp])
+    let command = rules[source[ip]];
+    if( typeof command === "function" ) { command() }
+    ip++;    
+  }
+
+  return result;
+};
+
+poohbear( 'LQTcQAP>pQBBTAI-PA-PPL+P<BVPAL+T+P>PL+PBLPBP<DLLLT+P' );
+let r = poohbear("++LQTT>W++LQTTWTNEP<NE");
+r
+
+//poohbear('+LTQII>+WN<P>+E')
+
+let s = "..WezWabEaWgogogoEzE--";
+let o = s.match(/W|E/g).reduce( (r, e) => [...r,[e,s.indexOf(e,r.length?r[r.length-1][1]+1:0)]] , [])
+o
+
+let o2 = o.reduce( (r, e) => e[0]=='W' ? [...r,e] : (()=>{ let l=r.pop();l.push(e);return [...r,l]})() , [] )
+o2
+
+let o3 = ( nx=0, stack=[] ) => s.match(/W|E/g).reduce( (r, e) => 
+{
+  nx = s.indexOf(e,nx+1);  
+  (e=="W") ? stack.push(nx) : r.push( [stack.pop(),nx] );
+  return r;
 }
+, [])
+let z = o3()
+z
+
+let test = o3 == [
+  [ 2,19 ],
+  [ 5,8 ],
+  [ 10,17 ]
+];
+test 
+
 
