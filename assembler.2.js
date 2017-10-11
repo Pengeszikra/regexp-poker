e3783ce4f56f99e0256bb1e76b0d8846880db3c2
Peter Vivo <petervivo@bitbucket.org>
2017-10-11 21:45:18 +0200

    python ref link

--- assembler.2.js
+++ assembler.2.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/assembler-interpreter-part-ii
 
+// http://www.onjava.com/pub/a/python/excerpt/PythonPocketRef/index.html
 
 assembler = (
     source, 
@@ -75,7 +76,57 @@ assembler = (
       div  a, 2
       ret
   `
-  assembler(program)
+  console.log(assembler(program))
+
+  program_gcd = `
+  mov   a, 81         ; value1
+  mov   b, 153        ; value2
+  call  init
+  call  proc_gcd
+  call  print
+  end
+  
+  proc_gcd:
+      cmp   c, d
+      jne   loop
+      ret
+  
+  loop:
+      cmp   c, d
+      jg    a_bigger
+      jmp   b_bigger
+  
+  a_bigger:
+      sub   c, d
+      jmp   proc_gcd
+  
+  b_bigger:
+      sub   d, c
+      jmp   proc_gcd
+  
+  init:
+      cmp   a, 0
+      jl    a_abs
+      cmp   b, 0
+      jl    b_abs
+      mov   c, a            ; temp1
+      mov   d, b            ; temp2
+      ret
+  
+  a_abs:
+      mul   a, -1
+      jmp   init
+  
+  b_abs:
+      mul   b, -1
+      jmp   init
+  
+  print:
+      msg   'gcd(', a, ', ', b, ') = ', c
+      ret
+  `
+  
+  console.log(assembler(program_gcd))
   
 
   //Test.assert_equals(, '(5+1)/2 = 3')  
