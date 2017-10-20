f1734ec66c0d468fdb2441acae4399bf4dc13dc5
Peter Vivo <peter.vivo@doclerholding.com>
2017-10-20 14:11:37 +0200

    nestSearch

--- tokenizer.js
+++ tokenizer.js
@@ -1,24 +1,45 @@
 // tokenizer
 
-let sor = 'eleje-->(--hello( nem baj ha van benne )--)<--->(..(--(+belso)--12--)..1)<-- end'
-const next = s => { let poz = s.search(/\(|\)/); return poz< 0 ? null : {poz,st:s[poz] == '('} }
+/*
+  const token = ( arr, nest = nesting( arr ) ) => 
+  [ ...nest.preNested, [ token( nest.firstGroup ) ] , token( nest.leftOver ) ];
+*/  
 
-const found = s => {
-  return s.replace(/(\(|\))/,' $1 ')
-          .split(/\b|\s+/)
-          .map( e => e.trim() )
-          .filter( e => e.length )
+
+let sor = 'eleje-->(--hello( nem baj ha van benne )--)<--->(..(--(+belso)--12--)..1)<-- end'
+
+const prepare = s => s.replace(/(\(|\))/g,' $1 ').split(/\b|\s+/).map( e => e.trim() ).filter( e => e.length );
+const nestSearch = (
+  arr,
+  pos = 0,
+  lvl = 0,  
+  {begin, end} = { begin: arr.indexOf('(',pos), end: arr.indexOf(')',pos) }
+) => {
+  let justLeftOver = begin == -1 && end == -1    
+  let isOpen = begin != -1 && begin < end
+  lvl += justLeftOver ? 0 : isOpen ? +1 : -1
+  pos ++
+  return {justLeftOver, isOpen, begin, end, pos, lvl }
+};
+
+const nesting = arr => {  
+  let nest = nestSearch(arr)
+  if( nest.justLeftOver ){ return arr }  
+  do {
+    nest = nestSearch(arr, nest.pos+1, nest.lvl )    
+  } while( nest.lvl >= 0 )  
+  return nest
 }
 
+const tokenizer = s => nesting( prepare(s) )
 
-console.log( next(sor) )
-console.log( found(sor) )
-console.log( JSON.stringify(found(sor)) )
-console.log( JSON.stringify(found('(())()')) )
+console.log( tokenizer('sor') )
+//console.log( JSON.stringify(found(sor)) )
+console.log( JSON.stringify(tokenizer('(())()')) )
 //console.log( found(found(sor)[1][0]) )
-//console.log( found(found(found(found(sor)[1][0])[1][0])) )
+console.log( '[`'+sor.replace(/\(/g, '`,[`' ).replace(/\)/g, '`],`' )+'`]' )
 
-
+const next = s => { let poz = s.search(/\(|\)/); return poz< 0 ? null : {poz,st:s[poz] == '('} }
 const foundEr = s => {  
   let lvl = 0  
   let po = 0
@@ -37,3 +58,6 @@ const foundEr = s => {
         foundEr(s.slice(po))
       ]
 }
+
+
+// console.log( next(sor) )
